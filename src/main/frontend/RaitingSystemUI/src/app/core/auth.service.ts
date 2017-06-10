99b20e30031283fc09f09dab01c6e396e0b5b9d7
Alex <alextazetdinov@mail.ru>
2017-06-10 21:03:03 +0300

    Add local-storage service.

--- src/main/frontend/RaitingSystemUI/src/app/core/auth.service.ts
+++ src/main/frontend/RaitingSystemUI/src/app/core/auth.service.ts
@@ -1,32 +1,44 @@
-import { Injectable } from '@angular/core';
-import {Http, RequestOptions,Headers, Response} from '@angular/http';
-import {Login} from '../login/login.model';
-import {Observable} from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/throw';
-import {HttpErrorHandlerService} from './http-error-handler.service';
+import {Injectable} from "@angular/core";
+import {Headers, Http, RequestOptions, Response} from "@angular/http";
+import {Login} from "../login/login.model";
+import {Observable} from "rxjs/Observable";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
+import "rxjs/add/observable/throw";
+import {HttpErrorHandlerService} from "./http-error-handler.service";
+import {LocalStorageService} from "./local-storage.service";
 
 @Injectable()
 export class AuthService {
 
   constructor(private http: Http,
-              private httpErrorHandler: HttpErrorHandlerService) { }
+              private httpErrorHandler: HttpErrorHandlerService,
+              private localStorageService: LocalStorageService) {
+  }
 
   authLogin(login: Login): Observable<boolean> {
     const query = `username=${login.username}&password=${login.password}&submit=${login.submit}`;
     let headers = new Headers();
     headers.append('Content-Type', 'application/x-www-form-urlencoded');
-    let options = new RequestOptions({ headers: headers });
-    return this.http.post('http://localhost:8080/login', query, options)
+    let options = new RequestOptions({headers: headers});
+    return this.http.post('/login', query, options)
       .map((response: Response) => {
-        if (response.json()) {
-          console.log(response.json());
-          // this.authenticate(response.json().data);
-          // this.isAuthenticated = true;
-        }
+      this.getUserInfo();
+
+
+        // if (response.json()) {
+        //   console.log(response.json());
+        //   // this.authenticate(response.json().data);
+        //   // this.isAuthenticated = true;
+        // }
         return response.json();
       })
       .catch(this.httpErrorHandler.handleError);
+  }
+
+  getUserInfo() {
+    this.http.get('/api/userInfo').map((response: Response) => {
+      return response.json();
+    })
   }
 }
