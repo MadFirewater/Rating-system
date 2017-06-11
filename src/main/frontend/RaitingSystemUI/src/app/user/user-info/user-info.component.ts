cf31832960ec5c8c5db25699d10fc49d1f6722d5
Alex <alextazetdinov@mail.ru>
2017-06-11 14:53:34 +0300

    Add logout button.

--- src/main/frontend/RaitingSystemUI/src/app/user/user-info/user-info.component.ts
+++ src/main/frontend/RaitingSystemUI/src/app/user/user-info/user-info.component.ts
@@ -1,23 +1,30 @@
 import {Component, OnInit} from "@angular/core";
 import {UserService} from "../user.service";
+import {AuthService} from "../../core/auth.service";
 
 @Component({
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
   styleUrls: ['./user-info.component.scss'],
-  providers: [UserService]
+  providers: [UserService, AuthService]
 })
 export class UserInfoComponent implements OnInit {
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private authService: AuthService) {
   }
-  username="";
+
+  username = "";
+
   ngOnInit() {
-    this.username=this.getUsername();
+    this.username = this.getUsername();
   }
 
   getUsername(): string {
     return this.userService.getUserName();
+  }
+
+  logout() {
+    this.authService.logout();
   }
 
 }
